7b91b18446e15490863b0173555df0a355b9dad7
migao <migao@pinduoduo.com>
2019-05-15 11:46:32 +0800

    递归版本汉诺塔完成

---
+++ src/main/java/com/migao/algorithm/stack/Hanoi.java
@@ -0,0 +1,82 @@
+package com.migao.algorithm.stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Hanoi {
+
+    private static final String LEFT = "left";
+
+    private static final String RIGHT = "right";
+
+    private static final String MIDDLE = "middle";
+
+    private Stack<Integer> left = new Stack<>();
+
+    private Stack<Integer> middle = new Stack<>();
+
+    private Stack<Integer> right = new Stack<>();
+
+    private Map<String, Stack<Integer>> nameMap = new HashMap<>();
+
+    public Hanoi(int num) {
+        if (num <= 0) {
+            throw new RuntimeException("num:" + num + " should be greater than 0");
+        }
+        nameMap.put(LEFT, left);
+        nameMap.put(MIDDLE, middle);
+        nameMap.put(RIGHT, right);
+        for (int i = 0; i < num; i++) {
+            left.push(num - i);
+        }
+    }
+
+    public void run() {
+        printAllStack();
+        if (left.empty()) {
+            return;
+        }
+        recursiveMove(left.size(), LEFT, RIGHT);
+    }
+
+    private void recursiveMove(int num, String from, String to) {
+        if (num <= 1) {
+            move(from, to);
+            return;
+        }
+        recursiveMove(num - 1, from, to);
+        move(from, MIDDLE);
+        recursiveMove(num - 1, to, from);
+        move(MIDDLE, to);
+        recursiveMove(num - 1, from, to);
+    }
+
+    private void move(String from, String to) {
+        if (MIDDLE.equals(from) || MIDDLE.equals(to)) {
+            directMove(from, to);
+        } else {
+            directMove(from, MIDDLE);
+            directMove(MIDDLE, to);
+        }
+    }
+
+    private void directMove(String from, String to) {
+        Stack<Integer> fromStack = nameMap.get(from);
+        Stack<Integer> toStack = nameMap.get(to);
+        if (toStack.empty() || toStack.peek() > fromStack.peek()) {
+            Integer value = fromStack.pop();
+            System.out.println(value + ":" + from + " -> " + to);
+            toStack.push(value);
+            printAllStack();
+            return;
+        }
+        throw new RuntimeException(from + ":" + fromStack.toString() + " -> " + to + ":" + toStack.toString());
+    }
+
+    private void printAllStack() {
+        System.out.println(LEFT + ":" + left.toString());
+        System.out.println(MIDDLE + ":" + middle.toString());
+        System.out.println(RIGHT + ":" + right.toString());
+    }
+}
---
+++ src/test/java/com/migao/algorithm/stack/HanoiTest.java
@@ -0,0 +1,29 @@
+package com.migao.algorithm.stack;
+
+import org.junit.Test;
+
+public class HanoiTest {
+
+    @Test(expected = RuntimeException.class)
+    public void createErrorTest() {
+        new Hanoi(0);
+    }
+
+    @Test
+    public void oneTest() {
+        Hanoi hanoi = new Hanoi(1);
+        hanoi.run();
+    }
+
+    @Test
+    public void twoTest() {
+        Hanoi hanoi = new Hanoi(2);
+        hanoi.run();
+    }
+
+    @Test
+    public void threeTest() {
+        Hanoi hanoi = new Hanoi(3);
+        hanoi.run();
+    }
+}
